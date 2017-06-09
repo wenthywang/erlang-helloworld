516f27ea6ad653bfa67b4f0b5459f4358dc219c2
wangwenhui <wangwenhui@jiaxincloud.com>
2017-06-09 10:12:22 +0800

    提交源码

---
+++ src/helloworld.erl
@@ -0,0 +1,14 @@
+%%%-------------------------------------------------------------------
+%%% @author Administrator
+%%% @copyright (C) 2017, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 08. 六月 2017 14:21
+%%%-------------------------------------------------------------------
+-module(helloworld).
+-author("Administrator").
+
+%% API
+-export([test/0]).
+test()->io:format("hello world").
